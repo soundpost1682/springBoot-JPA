06b12959cd972cc7d6e46032baf0ceb6e73cea75
soundpost1682 <guarneri1682@gmail.com>
2024-07-18 11:13:35 -0700

    set DB MySQL in Azure

--- src/main/java/com/example/demo/ItemController.java
+++ src/main/java/com/example/demo/ItemController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
     @GetMapping("/list")
-    String list(){
+    String list(Model model){
+        model.addAttribute("name", "pants!");
         return "list.html";
     }
 }
