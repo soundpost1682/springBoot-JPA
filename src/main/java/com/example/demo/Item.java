9492e88f42c007b9713a6f54c66b7e2f49c61f56
soundpost1682 <guarneri1682@gmail.com>
2024-07-23 01:19:38 -0700

    DB to website

--- src/main/java/com/example/demo/Item.java
+++ src/main/java/com/example/demo/Item.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
 import jakarta.persistence.*;
+import lombok.ToString;
+
 
 @Entity
+@ToString
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
-    @Column(nullable = false, unique = true)
     public String title;
     public Integer price;
+
+
 }
 
